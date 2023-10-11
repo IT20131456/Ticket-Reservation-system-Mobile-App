c6036bab8a62b4a49f3df24777fc4342023f879f
T Herath <hkalani00@gmail.com>
2023-10-11 22:41:18 +0530

    Booking modifications and remove with api call (tested)

--- app/src/main/java/com/example/mobileapp/api/ApiService.java
+++ app/src/main/java/com/example/mobileapp/api/ApiService.java
@@ -5,8 +5,10 @@ import com.example.mobileapp.data.model.TrainSchedule;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiService {
@@ -18,5 +20,11 @@ public interface ApiService {
 
     @GET("api/TicketBooking/History/{nic}")
     Call<List<Reservation>> getAllBookings(@Path("nic") String id);
+
+    @PUT("api/TicketBooking/{id}")
+    Call<Void> updateReservation(@Path("id") String id, @Body Reservation reservation);
+
+    @DELETE("api/TicketBooking/{id}")
+    Call<Void> removeReservation(@Path("id") String id);
 }
 
